54524fa7c86f74da859c6a734efb2a70bb7e9960
Niles Rowland <neterslandreau@gmail.com>
2017-08-03 13:30:23 -0400

    change location of binaries to environment variables.

--- config/snappy.php
+++ config/snappy.php
@@ -6,7 +6,7 @@ return array(
     'pdf' => array(
         'enabled' => true,
         // 'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
-        'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'binary'  => env(WKHTMLTOPDF, '/usr/local/bin/wkhtmltopdf'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
@@ -14,7 +14,7 @@ return array(
     'image' => array(
         'enabled' => true,
         // 'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltoimage-amd64'),
-        'binary'  => '/usr/local/bin/wkhtmltoimage',
+        'binary'  => env(WKHTMLTOIMAGE, '/usr/local/bin/wkhtmltoimage'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
